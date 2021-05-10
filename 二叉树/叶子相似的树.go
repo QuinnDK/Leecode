77d00204992c018fec3b9183ff7634ac808e5922
QuinnDK <18207205276@163.com>
2021-05-10 13:21:10 +0800

    no tips

---
+++ 二叉树/叶子相似的树.go
@@ -0,0 +1,38 @@
+package 二叉树
+
+import "sort"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func leafSimilar(root1, root2 *TreeNode) bool {
+	vals := []int{}
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		if node.Left == nil && node.Right == nil {
+			vals = append(vals, node.Val)
+			return
+		}
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root1)
+	vals1 := append([]int(nil), vals...)
+	vals = []int{}
+	dfs(root2)
+	if len(vals) != len(vals1) {
+		return false
+	}
+	for i, v := range vals1 {
+		if v != vals[i] {
+			return false
+		}
+	}
+	return true
+}
