77392fc6d13700a333971ae2992c35b1e0843642
QuinnDK <18207205276@163.com>
2021-05-17 14:37:03 +0800

    no tips

---
+++ 回溯/二叉树的层序遍历.go
@@ -0,0 +1,36 @@
+package 回溯
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		list := make([]int, 0)
+		l := len(queue)
+
+		for i := 0; i < l; i++ {
+			level := queue[0]
+			queue = queue[1:]
+			list = append(list, level.Val)
+			if level.Left != nil {
+				queue = append(queue, level.Left)
+			}
+			if level.Right != nil {
+				queue = append(queue, level.Right)
+			}
+		}
+		result = append(result, list)
+	}
+	return result
+}
