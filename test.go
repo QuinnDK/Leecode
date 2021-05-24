b32d0fa04177fbca183a194972914b1c950f20dc
QuinnDK <18207205276@163.com>
2021-05-24 18:17:01 +0800

    no tips

--- test.go
+++ test.go
@@ -1,9 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	for i:=range "6b4e03423667dbb73b6e15454f0eb1abd4597f9a1b078e3f5b5a6bc7"{
-		fmt.Println(i)
+	//for i:=range "6b4e03423667dbb73b6e15454f0eb1abd4597f9a1b078e3f5b5a6bc7"{
+	//	fmt.Println(i)
+	//}
+	//str1 := "The only person " +
+	//	"standing in your way " +
+	//	"is you"
+	//fmt.Println(str1)
+	//var a int =1
+	//str := fmt.Sprintf("%s%d%s", "format", a, "by fmt.Sprintf")
+	//fmt.Println(str)
+	//fmt.Printf("%s\n",str)
+	//
+	//str2:=[]string{"22","33","44"}
+	//str3:=strings.Join(str2,"")
+	//fmt.Println(str3,len(str3))
+
+	//var buf bytes.Buffer
+	//str:=[]string{"11"," 22","sss"}
+	//for i:=0;i<len(str);i++{
+	//	buf.WriteString(str[i])
+	//}
+	//fmt.Println(buf.String())
+
+	//var buf strings.Builder
+	//str:=[]string{"11"," 22","sss"}
+	//for i:=0;i<len(str);i++{
+	//	buf.WriteString(str[i])
+	//}
+	//fmt.Println(buf.String())
+
+	var buf strings.Builder
+	str := []byte{97, 65, 100, 68, 'z'}
+	for i := 0; i < len(str); i++ {
+		buf.WriteString(string(str[i]))
 	}
+	fmt.Println(buf.String())
 }
