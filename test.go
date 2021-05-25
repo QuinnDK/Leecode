46cec1a3597cc26da01273c54b0e4119db70e9c0
QuinnDK <18207205276@163.com>
2021-05-25 11:06:38 +0800

    no tips

--- test.go
+++ test.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//for i:=range "6b4e03423667dbb73b6e15454f0eb1abd4597f9a1b078e3f5b5a6bc7"{
 	//	fmt.Println(i)
@@ -38,4 +40,25 @@ func main() {
 	//}
 	//fmt.Println(buf.String())
 
+	a := []int{1, 2, 3}
+	b := a[:2]
+	a[0] = 10
+	//此时a和b共享底层数组
+	fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
+	fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
+	fmt.Println("-----------")
+	b = append(b, 999)
+	//虽然b append了1,但是没有超出cap，所以未进行内存重新分配
+	//等同于b[2]=999，因此a[2]一并被修改
+	fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
+	fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
+	fmt.Println("-----------")
+	a[2] = 555 //同上，未重新分配，所以，a[2] b[2]都被修改
+	fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
+	fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
+	fmt.Println("-----------")
+	b = append(b, 777) //超出了cap，这时候b进行重新分配,b[3]=777,cap(b)=6
+	a[2] = 666         //这时候a和b不再共享
+	fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
+	fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
 }
