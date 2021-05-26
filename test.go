42e834c1aecb2f0d3082dca6c2f90e21d8f3dd93
QuinnDK <18207205276@163.com>
2021-05-26 14:31:03 +0800

    no tips

--- test.go
+++ test.go
@@ -39,26 +39,31 @@ func main() {
 	//	buf.WriteString(string(str[i]))
 	//}
 	//fmt.Println(buf.String())
+	//
+	//a := []int{1, 2, 3}
+	//b := a[:2]
+	//a[0] = 10
+	////此时a和b共享底层数组
+	//fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
+	//fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
+	//fmt.Println("-----------")
+	//b = append(b, 999)
+	////虽然b append了1,但是没有超出cap，所以未进行内存重新分配
+	////等同于b[2]=999，因此a[2]一并被修改
+	//fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
+	//fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
+	//fmt.Println("-----------")
+	//a[2] = 555 //同上，未重新分配，所以，a[2] b[2]都被修改
+	//fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
+	//fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
+	//fmt.Println("-----------")
+	//b = append(b, 777) //超出了cap，这时候b进行重新分配,b[3]=777,cap(b)=6
+	//a[2] = 666         //这时候a和b不再共享
+	//fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
+	//fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
 
-	a := []int{1, 2, 3}
-	b := a[:2]
-	a[0] = 10
-	//此时a和b共享底层数组
-	fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
-	fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
-	fmt.Println("-----------")
-	b = append(b, 999)
-	//虽然b append了1,但是没有超出cap，所以未进行内存重新分配
-	//等同于b[2]=999，因此a[2]一并被修改
-	fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
-	fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
-	fmt.Println("-----------")
-	a[2] = 555 //同上，未重新分配，所以，a[2] b[2]都被修改
-	fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
-	fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
-	fmt.Println("-----------")
-	b = append(b, 777) //超出了cap，这时候b进行重新分配,b[3]=777,cap(b)=6
-	a[2] = 666         //这时候a和b不再共享
-	fmt.Println(a, "a cap:", cap(a), "a len:", len(a))
-	fmt.Println(b, "b cap:", cap(b), "b len:", len(b))
+	var a []int
+	a = append(a, 1, 3, 4, 4)
+
+	fmt.Println(a)
 }
--- test/1.go
+++ test/1.go
@@ -1,15 +1,30 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
+
+//func main() {
+//	if true {
+//		defer fmt.Printf("a")
+//	}else {
+//		defer fmt.Printf("b")
+//	}
+//
+//	fmt.Printf("c")
+//}
+
+const (
+	a = iota
+	b = iota
+)
+const (
+	name = "name" //iota已经开始计数，const每一行iota计数一次
+	c    = iota   //const关键词出现就会被重置
+	d    = iota
 )
 
 func main() {
-	if true {
-		defer fmt.Printf("a")
-	}else {
-		defer fmt.Printf("b")
-	}
-
-	fmt.Printf("c")
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 }
