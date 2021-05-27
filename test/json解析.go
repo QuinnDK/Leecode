344674dd4127b2c7550059600ea1418802ab76f1
QuinnDK <18207205276@163.com>
2021-05-27 21:27:16 +0800

    no tips

---
+++ test/json解析.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type People struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	js := `{"name":"seekload"}`
+	var p People
+	err := json.Unmarshal([]byte(js), &p)
+	if err != nil {
+		fmt.Println("err: ", err)
+		return
+	}
+	fmt.Println(p)
+}
---
+++ 字符串/最多中转k次的数量.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var n int
+	var route []string
+	//var x,y int
+	//var k int
+	var s string
+
+	fmt.Scanf("%d", &n)
+	//fmt.Println(n)
+	var buf strings.Builder
+	for i := 0; i < n; i++ {
+		buf.WriteString()
+	}
+	//for i:=0;i<n;i++{
+	//	fmt.Scanf("%s",&s)
+	//	route=append(route,s)
+	//}
+	fmt.Println(route)
+	//fmt.Scanf("%d %d",&x,&y)
+	//fmt.Println(x,y)
+	//fmt.Scanf("%d",&k)
+	//fmt.Println(k)
+}
