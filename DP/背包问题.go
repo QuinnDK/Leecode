5743c15c6d6e972696c8e3d962064e6aedd8e42f
QuinnDK <18207205276@163.com>
2021-06-18 12:25:51 +0800

    no tips

--- DP/背包问题.go
+++ DP/背包问题.go
@@ -26,23 +26,53 @@ func main() {
 		V = append(V, num)
 	}
 	fmt.Println(V)
-
-	/*  for{
-	        _,err:=fmt.Scan(&num)
-	        if err==io.EOF{
-	            break
-	        }
-	        w=append(w, num)
-	    }
-	    for{
-	        _,err:=fmt.Scan(&num)
-	        if err==io.EOF{
-	            break
-	        }
-	        v=append(v, num)
-	    }*/
 	res := chenzai(W, V, c)
 	fmt.Print(res)
+}
+
+//func chenzai(w, v []int, c int) int {
+//	lenth := len(w)
+//	if lenth == 0 {
+//		return 0
+//	}
+//	memo := make([]int, c+1)
+//	if w[0] <= c {
+//		for i := w[0]; i <= c; i++ {
+//			memo[i] = v[0]
+//		}
+//	}
+//	for i := 1; i < lenth; i++ {
+//		for j := c; j >= w[i]; j-- {
+//			memo[j] = max(memo[j], v[i]+memo[j-w[i]])
+//		}
+//	}
+//	return memo[c]
+//}
+
+func chenzai(wight, value []int, W int) int {
+	N := len(wight)
+	dp := make([][]int, N+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, W+1)
+	}
+
+	for i := 1; i <= N; i++ {
+		for j := 1; j <= W; j++ {
+			if j-wight[i-1] < 0 {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = max(dp[i-1][j], value[i-1]+dp[i-1][j-wight[i-1]])
+			}
+		}
+	}
+	return dp[N][W]
+
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 //// BagNormalLess solve bag normal problem
@@ -66,27 +96,3 @@ func main() {
 //	}
 //	return M[maxWeight]
 //}
-func chenzai(w, v []int, c int) int {
-	lenth := len(w)
-	if lenth == 0 {
-		return 0
-	}
-	memo := make([]int, c+1)
-	if w[0] <= c {
-		for i := w[0]; i <= c; i++ {
-			memo[i] = v[0]
-		}
-	}
-	for i := 1; i < lenth; i++ {
-		for j := c; j >= w[i]; j-- {
-			memo[j] = max(memo[j], v[i]+memo[j-w[i]])
-		}
-	}
-	return memo[c]
-}
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
